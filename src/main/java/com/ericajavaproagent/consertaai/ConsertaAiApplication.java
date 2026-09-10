598017d3ee7d25e65018029b1401fc99a60927fb
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-10 16:33:47 -0300

    fix: move ConsertaAIAplication para o pacote raiz e ativa scanBasePackages

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -2,11 +2,16 @@ package com.ericajavaproagent.consertaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Classe principal de inicializacao da aplicacao ConsertaAI.
+ * Mapeamento explicito de componentes, repositorios e entidades para deploy em container.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ericajavaproagent.consertaai")
+@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai")
+@EntityScan(basePackages = "com.ericajavaproagent.consertaai")
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
