3bafd5f3945e308a4eb1534f6eb22f1898a4ef0c
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-11 14:03:07 -0300

    Garante anotacoes de componentes na camada de persistencia

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -2,12 +2,8 @@ package com.ericajavaproagent.consertaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.ericajavaproagent.consertaai")
-@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
-@EntityScan(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
+@SpringBootApplication
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
