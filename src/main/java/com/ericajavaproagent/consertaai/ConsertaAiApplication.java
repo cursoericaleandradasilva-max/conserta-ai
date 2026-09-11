11b7f7e3f8aba60e9f89d8dfde55857a63e54a33
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-11 14:25:37 -0300

    Usa basePackageClasses para garantir escaneamento do repositorio JPA

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -1,13 +1,15 @@
 package com.ericajavaproagent.consertaai;
 
+import com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence.OcorrenciaJpaEntity;
+import com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence.SpringDataOcorrenciaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.ericajavaproagent.consertaai")
-@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
-@EntityScan(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = SpringDataOcorrenciaRepository.class)
+@EntityScan(basePackageClasses = OcorrenciaJpaEntity.class)
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
