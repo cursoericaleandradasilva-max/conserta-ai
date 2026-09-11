b5437bdc0b8c10c6562f4876025f45b9868d7f8d
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-11 15:14:01 -0300

    Atualiza ConsertaAiApplication com mapeamento de repositórios JPA

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -2,14 +2,8 @@ package com.ericajavaproagent.consertaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.ericajavaproagent.consertaai")
-@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai")
-@EntityScan(basePackages = "com.ericajavaproagent.consertaai")
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
