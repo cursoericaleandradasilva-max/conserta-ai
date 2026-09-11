3198dac2faea74336c1116fd80d1dcd4e97bccd5
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-11 15:05:25 -0300

    Adiciona ComponentScan, EnableJpaRepositories e EntityScan globais na classe principal

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -3,11 +3,13 @@ package com.ericajavaproagent.consertaai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
-@EntityScan(basePackages = "com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence")
+@ComponentScan(basePackages = "com.ericajavaproagent.consertaai")
+@EnableJpaRepositories(basePackages = "com.ericajavaproagent.consertaai")
+@EntityScan(basePackages = "com.ericajavaproagent.consertaai")
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
