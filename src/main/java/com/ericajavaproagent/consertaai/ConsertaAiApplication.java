4127eba859e74fe78b2e43c37cc14f34bc145748
cursoericaleandradasilva-max <cursoericaleandradasilva@gmail.com>
2026-09-11 14:39:14 -0300

    Simplifica classe principal para garantir compilacao limpa

--- src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
+++ src/main/java/com/ericajavaproagent/consertaai/ConsertaAiApplication.java
@@ -1,15 +1,9 @@
 package com.ericajavaproagent.consertaai;
 
-import com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence.OcorrenciaJpaEntity;
-import com.ericajavaproagent.consertaai.infrastructure.adapter.out.persistence.SpringDataOcorrenciaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = SpringDataOcorrenciaRepository.class)
-@EntityScan(basePackageClasses = OcorrenciaJpaEntity.class)
 public class ConsertaAiApplication {
 
     public static void main(String[] args) {
